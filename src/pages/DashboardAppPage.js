1d25236c7415cbbf12a3073d89422ea07331b28e
himanshu091 <himanshu091rawat@gmail.com>
2022-12-03 23:59:05 +0530

    Added modal for survey ui

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography,Stack,Button } from '@mui/material';
+import { Grid, Container, Typography,Stack,Button ,Modal,Box,TextField} from '@mui/material';
 import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
 // mock
 import PRODUCTS from '../_mock/products';
@@ -22,9 +22,22 @@ import {
 } from '../sections/@dashboard/app';
 
 // ----------------------------------------------------------------------
-
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 export default function DashboardAppPage() {
   const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
@@ -37,7 +50,7 @@ export default function DashboardAppPage() {
           Hi, Welcome to RealReview
         </Typography>
         <Stack direction="row" alignItems={"flex-end"} justifyContent={"flex-end"} spacing={1} flexShrink={0} sx={{ my: 2 }}>
-         <Button variant="contained">Create Survey</Button>
+         <Button onClick={handleOpen} variant="contained">Create Survey</Button>
             
           </Stack>
         <Grid container spacing={3} sx={{ my: 2 }}>
@@ -58,6 +71,23 @@ export default function DashboardAppPage() {
           <ProductList products={PRODUCTS} />
         </Grid>
       </Container>
+
+      <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style} >
+  <Typography id="modal-modal-title" variant="h6" component="h2">
+      Create Your Survey
+    </Typography>
+    <TextField fullWidth label="Name" id="name" />
+    <TextField type="number" fullWidth label="Partcipants" id="name" />
+
+  <TextField fullWidth label="Type Your Question" id="fullWidth" />
+  </Box>
+</Modal>
     </>
   );
 }
