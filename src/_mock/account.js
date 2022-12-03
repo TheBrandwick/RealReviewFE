f8c537a63d50647eeb37169ad516d2e4351d7a0c
himanshu091 <himanshu091rawat@gmail.com>
2022-12-04 03:19:16 +0530

    added fixes

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------
 
 const account = {
-  displayName: 'Jaydon Frankie',
+  displayName: '0X464646546546g',
   email: 'demo@minimals.cc',
   photoURL: '/assets/images/avatars/avatar_default.jpg',
 };
